50bb33640edf0b92dd897018d84724ad6bf76175
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-27 00:24:18 -0500

    Solved 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(pString, numRepeat) {
+    let repeatedString = "";
+    if (numRepeat < 0) {
+        repeatedString = "ERROR";
+    } else {
+        for (let i = 0; i < numRepeat; i++) {
+            repeatedString += pString;
+        }
+    }
 
+    return repeatedString;
 };
 
 // Do not edit below this line
