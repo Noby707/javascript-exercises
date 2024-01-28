2ca00937f7cdc0689ae72beb2363c72633c13783
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-27 23:01:15 -0500

    Solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    // Checks for non nums
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
+    // Checks for negatives
+    if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+    
 
+    if (n1 > n2) {
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    // let sum = 0;
+    // for(let i = n1; i <= n2; i++) {
+    //     sum += i;
+    // }
+
+
+
+    let sum = 0;
+    n1--;
+    let sum_n1 = (n1 * (n1+1)) / 2;
+    let sum_n2 = (n2 * (n2+1)) / 2;
+
+    sum = sum_n2 - sum_n1;
+
+    return sum;
 };
 
 // Do not edit below this line
