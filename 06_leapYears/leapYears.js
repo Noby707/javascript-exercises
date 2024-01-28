4f3e17f6d4c7fbd9dd3f6a9536b8fbd3f0a727d5
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-27 23:09:55 -0500

    Solved 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapBool = false;
+    // Check for divisibility by 4
+    if (year % 4 == 0) {
+        leapBool = true;
+    }
+    // Check for divisibility by 400 and 100
+    if (year % 100 == 0) {
+        leapBool = false;
 
+        if (year % 400 == 0) {
+            leapBool = true;
+        }
+    }
+
+    return leapBool;
 };
 
 // Do not edit below this line
