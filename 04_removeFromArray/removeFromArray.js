530fe10f67fc376c7e607d0be7aef85f8ef7e4a5
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-27 22:42:42 -0500

    Solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...argArray) {
+    let newArray = arr.filter((el) => !argArray.includes(el));
 
+    return newArray;
 };
 
 // Do not edit below this line
