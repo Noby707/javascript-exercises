25b792127d80b082153d0b0c976d0d23721d9dea
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-04-21 02:33:10 -0400

    Solved Excercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n < 3) {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2); // fib(1) + fib(0) + fib(1)
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
