9d7d6e9a80a79289503939e00ed7c560bac2c877
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-04-21 03:01:57 -0400

    Solved the excercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+// const getTheTitles = function(books) {
+//     let arr = [];
+//     books.forEach(element => {
+//         arr.push(element.title);
+//     });
+//     return arr;
+// };
 
+
+const getTheTitles = function(books) {
+    let arr = books.map(el => el.title);
+    return arr;
 };
 
 // Do not edit below this line
